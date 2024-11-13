4ad97f2da41aad79f5c1f6961a57a4eeb7bb9718
emiliano <echiper.dev@gmail.com>
2024-11-13 21:18:52 +0200

    Add left movement logic to Ship class

--- ship.py
+++ ship.py
@@ -17,10 +17,13 @@ class Ship:
 
         # Movement flag; starts with a stationary ship.
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """Update the ship's position beased on the movement flag."""
         if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
             self.rect.x += 1
 
     def blitme(self):
