7a311d80a5a504959095dd171ddb35d985bacd0d
emiliano <echiper.dev@gmail.com>
2024-11-13 21:22:32 +0200

    Add movement logic to events helper function of main program

--- ship.py
+++ ship.py
@@ -24,7 +24,7 @@ class Ship:
         if self.moving_right:
             self.rect.x += 1
         if self.moving_left:
-            self.rect.x += 1
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
