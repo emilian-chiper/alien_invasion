a3026e07a3450082958aab484d173417c592ce30
emiliano <echiper.dev@gmail.com>
2024-11-14 21:13:13 +0200

    Limit number of available bullets to 3

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 2.0
-        self.bullet_width = 3
+        self.bullet_width = 4
         self.bullet_height = 15
         self.bullet_color = (2, 209, 250)
+        self.bullets_allowed = 3
